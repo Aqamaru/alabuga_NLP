27ba8690d8467c93ed67dca2ccae0573c49e373a
Evdokiya01 <95077590+Evdokiya01@users.noreply.github.com>
2024-07-02 15:00:43 +0300

    ршощз
    н89г0ш

--- src/bot.py
+++ src/bot.py
@@ -1,115 +1,88 @@
-import keyboards as Keyboards
-from text import Text
-from telebot import TeleBot
-from telebot.types import Message
-
-BOT : TeleBot = TeleBot("7099811190:AAEwZY5cHq4aiuI7YvBt__uTwBTevQB_QCs")
-
-companies: list[str] = []
-
-@BOT.message_handler(commands = ['start'])
-def on_start(msg: Message) -> None: 
-
-    BOT.send_message(msg.chat.id, text = Text.START_MESSAGE,
-                     reply_markup = Keyboards.START)     
-
-@BOT.message_handler(commands = ['help'])
-def on_help(msg: Message) -> None:
-
-    msg.text = msg.text.split("/help ")
-    
-    match msg.text:
-
-        case "companies":
-            BOT.send_message(msg.chat.id, text = Text.HELP_CMD_COMPANIES,
-                             reply_markup = Keyboards.START)
-            return
-
-        case "analysis":
-            BOT.send_message(msg.chat.id, text = Text.HELP_CMD_ANALYSIS,
-                             reply_markup = Keyboards.START)
-            return
-
-        case _:
-            BOT.send_message(msg.chat.id, text = Text.HELP_BASE,
-                             reply_markup = Keyboards.HELPS)
-            return
-
-@BOT.message_handler(commands = ['companies'])
-def on_companies(msg: Message) -> None:
-    global companies 
-    companies = msg.text.replace("/companies ", "").split()
-    BOT.send_message(msg.chat.id, text = Text.SET_COMPANIES + " ".join(companies),
-                     reply_markup = Keyboards.START)
-    
-@BOT.message_handler(commands= ['analysis'])
-def on_analysis(msg: Message) -> None:
-    pass
-
-@BOT.message_handler(content_types=['text'])
-def on_message(msg: Message) -> None:
-
-    match msg.text:
-
-        case Text.ANALYSIS_BTN:
-            BOT.send_message(msg.chat.id, text = Text.ANALYSIS,
-                             reply_markup = Keyboards.ANALYSIS)
-            return
-        
-        case Text.BACK_TO_MAIN_MENU_BTN:
-            BOT.send_message(msg.chat.id, text = Text.BACK_TO_MAIN_MENU,
-                             reply_markup = Keyboards.START)
-            return
-
-        case Text.HELP_BTN:
-            BOT.send_message(msg.chat.id, text = Text.HELP,
-                             reply_markup = Keyboards.HELPS)
-            return
-        
-        case Text.CONTACTS_BTN:
-            BOT.send_message(msg.chat.id, text = Text.CONTACTS,
-                             reply_markup = Keyboards.START)
-            return
-        
-        case Text.USAGE_BTN:
-            BOT.send_message(msg.chat.id, text = Text.USAGE,
-                             reply_markup = Keyboards.START)
-            return
-        
-        case Text.SET_COMPANIES_BTN: # Кнопка в главном меню, добавляет в базу новую компанию для выбора
-            message = BOT.send_message(msg.chat.id, text = Text.SET_COMPANIES)
-            BOT.register_next_step_handler(message = message, callback = get_companies)
-            return
-        
-        case Text.SET_COMPANIES_ANALYSIS_BTN: # Тут в аналитике выбирается компания, следом необходимо будет ввести ссылку на статью
-            message = BOT.send_message(msg.chat.id, text = Text.SET_COMPANIES_ANALYSIS,
-                                        reply_markup = Keyboards.ANALYSIS)
-            
-            # вывод кнопок с компаниями, после вывод просьбы вывести статью => 
-            # => BOT.send_message(message.chat.id, text = Text.SEND_AN_ARTICLE) 
-
-            return
-        
-
-        case _:
-            BOT.send_message(msg.chat.id, text = Text.UNKNOWN_COMMAND,
-                             reply_markup = Keyboards.START)
-            return
-<<<<<<< HEAD
-        
-def get_companes(message: Message) -> None:
-    global COMPANIES 
-    COMPANIES +=  message.text.split()
-
-    BOT.send_message(message.chat.id, text = Text.GET_COMPANES + " " +  " ".join(COMPANIES))
-
-=======
-       
-def get_companies(message: Message) -> None:
-    global companies 
-    companies = message.text.split()
-    BOT.send_message(message.chat.id, text = Text.GET_COMPANIES + " " +  " ".join(COMPANIES))
->>>>>>> 23257666319246fa8066ba0093e8df61915e7231
-
+import keyboards as Keyboards 
+from text import Text 
+from telebot import TeleBot, types 
+from telebot.types import Message 
+ 
+ 
+BOT : TeleBot = TeleBot("7099811190:AAEwZY5cHq4aiuI7YvBt__uTwBTevQB_QCs") 
+ 
+COMPANIES : list[str] = [] 
+ 
+ 
+@BOT.message_handler(commands = ['start']) 
+def on_start(msg: Message) -> None:  
+ 
+    BOT.send_message(msg.chat.id, text = Text.START_MESSAGE, 
+                     reply_markup = Keyboards.START) 
+ 
+@BOT.message_handler(commands = ['help']) 
+def on_help(msg: Message) -> None: 
+    pass 
+ 
+@BOT.message_handler(commands = ['companies']) 
+def on_companies(msg: Message) -> None: 
+    pass 
+ 
+@BOT.message_handler(commands= ['analysis']) 
+def on_analysis(msg: Message) -> None: 
+ 
+    BOT.send_message(msg.chat.id, text = msg.text) 
+ 
+@BOT.message_handler(content_types=['text']) 
+def on_message(msg: Message) -> None: 
+ 
+    match msg.text: 
+ 
+        case Text.ANALYSIS_BTN: 
+            BOT.send_message(msg.chat.id, text = Text.ANALYSIS, 
+                             reply_markup = Keyboards.ANALYSIS) 
+            return 
+         
+        case Text.BACK_TO_MAIN_MENU_BTN: 
+            BOT.send_message(msg.chat.id, text = Text.BACK_TO_MAIN_MENU, 
+                             reply_markup = Keyboards.START) 
+            return 
+ 
+        case Text.HELP_BTN: 
+            BOT.send_message(msg.chat.id, text = Text.HELP, 
+                             reply_markup = Keyboards.HELPS) 
+            return 
+         
+        case Text.CONTACTS_BTN: 
+            BOT.send_message(msg.chat.id, text = Text.CONTACTS, 
+                             reply_markup = Keyboards.START) 
+            return 
+         
+        case Text.USAGE_BTN: 
+            BOT.send_message(msg.chat.id, text = Text.USAGE, 
+                             reply_markup = Keyboards.START) 
+            return 
+         
+        case Text.SET_COMPANIES_BTN: # Кнопка в главном меню, добавляет в базу новую компанию для выбора 
+            message = BOT.send_message(msg.chat.id, text = Text.SET_COMPANIES) 
+            BOT.register_next_step_handler(message = message, callback=get_companes) 
+            return 
+         
+        case Text.SET_COMPANIES_ANALYSIS_BTN: # Тут в аналитике выбирается компания, следом необходимо будет ввести ссылку на статью 
+            message = BOT.send_message(msg.chat.id, text = Text.SET_COMPANIES_ANALYSIS, 
+                                        reply_markup = Keyboards.ANALYSIS) 
+             
+            # вывод кнопок с компаниями, после вывод просьбы вывести статью =>  
+            # => BOT.send_message(message.chat.id, text = Text.SEND_AN_ARTICLE)  
+ 
+            return 
+         
+ 
+        case _: 
+            BOT.send_message(msg.chat.id, text = Text.UNKNOWN_COMMAND, 
+                             reply_markup = Keyboards.START) 
+            return 
+         
+def get_companes(message: Message) -> None: 
+    global COMPANIES  
+    COMPANIES +=  message.text.split() 
+ 
+    BOT.send_message(message.chat.id, text = Text.GET_COMPANES + " " +  " ".join(COMPANIES)) 
+ 
+ 
 BOT.infinity_polling()
-
--- src/keyboards.py
+++ src/keyboards.py
@@ -1,25 +1,22 @@
-from text import Text
-from telebot.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-
-START = ReplyKeyboardMarkup(resize_keyboard = True)\
-        .add(*(KeyboardButton(i) for i in (
-            Text.ANALYSIS_BTN,
-            Text.HELP_BTN,
-<<<<<<< HEAD
-            Text.SET_COMPANIES_BTN,
-        )))
-=======
+from text import Text 
+from telebot import types 
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove 
+ 
+START = ReplyKeyboardMarkup()\
+        .add(*(KeyboardButton(i) for i in ( 
+            Text.ANALYSIS_BTN, 
+            Text.HELP_BTN, 
+            Text.SET_COMPANIES_BTN, 
         ))) 
->>>>>>> 23257666319246fa8066ba0093e8df61915e7231
-
-ANALYSIS = ReplyKeyboardMarkup(resize_keyboard = True)\
-        .add(*(KeyboardButton(i) for i in (
-            Text.SET_COMPANIES_ANALYSIS,
-            Text.BACK_TO_MAIN_MENU_BTN,
-         )))
-
+ 
+ANALYSIS = ReplyKeyboardMarkup()\
+        .add(*(KeyboardButton(i) for i in ( 
+            Text.SET_COMPANIES_ANALYSIS, 
+            Text.BACK_TO_MAIN_MENU_BTN, 
+         ))) 
+ 
 HELPS = ReplyKeyboardMarkup()\
-        .add(*(KeyboardButton(i) for i in (
-            Text.CONTACTS_BTN,
-            Text.USAGE_BTN,
+        .add(*(KeyboardButton(i) for i in ( 
+            Text.CONTACTS_BTN, 
+            Text.USAGE_BTN, 
          )))
